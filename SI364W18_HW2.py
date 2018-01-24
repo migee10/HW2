070146cf834325182ba1c24808f8f825f8641eae
Michele Gee <migee@umich.edu>
2018-01-24 11:42:34 -0500

    test

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -2,6 +2,7 @@
 ## Winter 2018
 ## HW 2 - Part 1
 
+#Michele Gee
 ## This homework has 3 parts, all of which should be completed inside this file (and a little bit inside the /templates directory).
 
 ## Add view functions and any other necessary code to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
